262376986f2df1de77d731cedcb0b2815ea6d47d
Nebojša Jakovljević <nebojsa@nebjak.net>
2020-06-11 21:56:40 +0200

    added jsdocs

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ const data = JSON.parse(
   fs.readFileSync(path.join(__dirname, 'data/serbia_zip_codes.json'), 'utf8')
 );
 
+/**
+ * Get all dataset
+ * @returns {{city: string, zip_code: string}[]}
+ */
 function getAllSync() {
   return data;
 }
 
+/**
+ * Search by city name
+ * @param {string} city
+ * @returns {{city: string, zip_code: string}[]}
+ */
 function findByCitySync(city) {
   return data.filter(function (d) {
     return d.city.match(RegExp(city, 'i'));
   });
 }
 
+/**
+ * Search by zip code
+ * @param {string | number} zip
+ * @returns {{city: string, zip_code: string}[]}
+ */
 function findByZipSync(zip) {
   return data.filter(function (d) {
     return d.zip_code.match(RegExp(zip, 'i'));
